161d61f4ddefacd417a8f981dbe23ad13299e25a
Hossain Ahamed <hossainahamed6872@gmail.com>
2022-07-13 03:27:39 +0600

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const express = require("cors");
+
 require('dotenv').config()
 const app = express()
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
-
+app.use(cors());
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', require('./src/routes'))
